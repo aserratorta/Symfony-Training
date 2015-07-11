e549f641479e46be29f7dab198fa76c1596bb0bf
Anton Serra Torta <aserratorta@gmail.com>
2015-07-12 01:58:47 +0200

    2.3 librosweb

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
+// index.php
+
+// carga e inicia algunas librerías globales
 require_once 'model.php';
+require_once 'controllers.php';
 
-$posts = get_all_posts();
-
-require 'templates/list.php';
+// encamina la petición internamente
+$uri = $_SERVER['REQUEST_URI'];
+if ($uri == '/index.php') {
+    list_action();
+} elseif ($uri == '/index.php/show' && isset($_GET['id'])) {
+    show_action($_GET['id']);
+} else {
+    header('Status: 404 Not Found');
+    echo '<html><body><h1>Page Not Found</h1></body></html>';
+}
