6b7aa17f2aefb307a60019cccca73e1821b336f2
aserratorta <aserratorta@gmail.com>
2015-10-07 16:40:47 +0200

    finish otional extension twig

--- src/TestBundle/Twig/Extension/SymfonyExtension.php
+++ src/TestBundle/Twig/Extension/SymfonyExtension.php
@@ -14,4 +14,25 @@ class SymfonyExtension extends \Twig_Extension
             'discount' => new \Twig_Function_Method($this, 'discount')
         );
     }
+
+    public function discount($price, $discount, $decimals=0)
+    {
+        if (!is_numeric($price) || !is_nemeric($discount)) {
+            return '-';
+        }
+
+        if ($discount == 0 || $discount ==null) {
+            return 'o%';
+        }
+
+        $original_price = $price + $discount;
+        $percentage= ($discount/$original_price)*100;
+
+        return '-'.number_format($percentage, $decimals).'%';
+    }
+
+    public function getName()
+    {
+        return 'symfony';
+    }
 }
